1c1e2cfbd0016fae24391bea46871d257c7d5be3
regenthor <sezergokalp@hotmail.com>
2024-12-03 12:20:53 +0300

    health check

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ import express from 'express';
 import reflectionRoutes from './routes/reflectionRoutes.js';
 import 'dotenv/config';
 const app = express();
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 'OK'
+    });
+});
 app.use(express.json());
 app.use(reflectionRoutes);
 
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT} :)`);
 });
